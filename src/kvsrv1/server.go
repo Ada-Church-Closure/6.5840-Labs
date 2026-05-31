cd28f30f12c4fad83af1d9563b8826f50076737a
quanyeyang <quanyemostima@gmail.com>
2026-05-31 14:52:21 +0800

    lab2：实现单机线性版本的kv server
    Signed-off-by: quanyeyang <quanyemostima@gmail.com>

--- src/kvsrv1/server.go
+++ src/kvsrv1/server.go
@@ -6,7 +6,7 @@ import (
 
 	"6.5840/kvsrv1/rpc"
 	"6.5840/labrpc"
-	"6.5840/tester1"
+	tester "6.5840/tester1"
 )
 
 const Debug = false
@@ -18,23 +18,45 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
+type Tuple struct {
+	value   string
+	version rpc.Tversion
+}
 
+// server侧需要持有什么？
+// 利用什么数据结构？
+
+// 注意引用指针和修改版本号的问题
 type KVServer struct {
-	mu sync.Mutex
-
-	// Your definitions here.
+	mu    sync.Mutex
+	cache map[string]Tuple
 }
 
 func MakeKVServer() *KVServer {
 	kv := &KVServer{}
-	// Your code here.
+	kv.cache = make(map[string]Tuple)
 	return kv
 }
 
 // Get returns the value and version for args.Key, if args.Key
 // exists. Otherwise, Get returns ErrNoKey.
 func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key := args.Key
+	tuple, ok := kv.cache[key]
+
+	// 不存在
+	if !ok {
+		reply.Err = rpc.ErrNoKey
+		return
+	}
+
+	// 存在
+	reply.Value = tuple.value
+	reply.Version = tuple.version
+	reply.Err = rpc.OK
 }
 
 // Update the value for a key if args.Version matches the version of
@@ -42,10 +64,43 @@ func (kv *KVServer) Get(args *rpc.GetArgs, reply *rpc.GetReply) {
 // If the key doesn't exist, Put installs the value if the
 // args.Version is 0, and returns ErrNoKey otherwise.
 func (kv *KVServer) Put(args *rpc.PutArgs, reply *rpc.PutReply) {
-	// Your code here.
+	// 先上锁，为了线性化
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	key := args.Key
+	tuple, ok := kv.cache[key]
+
+	// 不存在
+	if !ok {
+		// version不为0,err
+		if args.Version != 0 {
+			reply.Err = rpc.ErrNoKey
+			return
+		}
+		// version为0：创建 key，server 上 version 变为 1
+		kv.cache[key] = Tuple{
+			value:   args.Value,
+			version: 1,
+		}
+		reply.Err = rpc.OK
+		return
+	}
+
+	// 存在
+	// 版本号相同
+	if args.Version == tuple.version {
+		kv.cache[key] = Tuple{
+			value:   args.Value,
+			version: tuple.version + 1,
+		}
+		reply.Err = rpc.OK
+		return
+	}
+
+	// 版本号不同
+	reply.Err = rpc.ErrVersion
 }
-
-
 
 // You can ignore all arguments; they are for replicated KVservers
 func StartKVServer(tc *tester.TesterClnt, ends []*labrpc.ClientEnd, gid tester.Tgid, srv int, persister *tester.Persister) []any {
